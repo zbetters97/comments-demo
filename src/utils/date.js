5220edb78f2f74aaba3d83ee84a6f64e19c60922
Zachary Betters <100448323+zbetters97@users.noreply.github.com>
2025-03-07 09:16:49 -0500

    0.0.4 Update -Updated date.js to be grammatically correct on single time values

--- src/utils/date.js
+++ src/utils/date.js
@@ -8,29 +8,29 @@ export function getTimeSince(date) {
 
   const minutes = Math.round(seconds / 60);
   if (minutes < 60) {
-    return minutes + " minutes ago";
+    return minutes < 2 ? "A minute ago" : minutes + " minutes ago";
   }
 
   const hours = Math.round(minutes / 60);
   if (hours < 24) {
-    return hours + " hours ago";
+    return hours < 2 ? "An hour ago" : hours + " hours ago";
   }
 
   const days = Math.round(hours / 24);
   if (days < 7) {
-    return days + " days ago";
+    return days < 2 ? "A day ago" : days + " days ago";
   }
 
   const weeks = Math.round(days / 7);
   if (weeks < 4) {
-    return weeks + " weeks ago";
+    return weeks < 2 ? "A week ago" : weeks + " weeks ago";
   }
 
   const months = Math.round(days / 30.44);
   if (months < 12) {
-    return months + " months ago";
+    return months < 2 ? "A month ago" : months + " months ago";
   }
 
   const years = Math.round(months / 12);
-  return years + " years ago";
+  return years < 2 ? "A year ago" : years + " years ago";
 }
