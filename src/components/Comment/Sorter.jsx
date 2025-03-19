86d781e2bfbc5a2b1569685577b949164faae320
Zachary Betters <100448323+zbetters97@users.noreply.github.com>
2025-03-19 12:21:22 -0400

    0.2.1 Update -Improved Comment Sorter efficiency

--- src/components/Comment/Sorter.jsx
+++ src/components/Comment/Sorter.jsx
@@ -6,45 +6,49 @@ export default function Sorter({ comments, setComments }) {
   const [showSort, setShowSort] = useState(false);
   const sorterRef = useRef(null);
 
+  const sortOptions = [
+    { label: "Newest", value: "newest" },
+    { label: "Oldest", value: "oldest" },
+    { label: "Best", value: "best" },
+    { label: "Controversial", value: "worst" },
+    { label: "Replied to", value: "replies" },
+  ];
+
   function sortComments(sortValue) {
     const sortedComments = comments.sort((a, b) => {
-      if (sortValue === "newest") {
-        return b.createdAt - a.createdAt;
-      }
+      switch (sortValue) {
+        case "newest":
+          return b.createdAt - a.createdAt;
 
-      if (sortValue === "oldest") {
-        return a.createdAt - b.createdAt;
-      }
+        case "oldest":
+          return a.createdAt - b.createdAt;
 
-      // Sort by likes, then by date, then by userId
-      if (sortValue === "best") {
-        return (
-          b.likes.length - a.likes.length ||
-          b.createdAt - a.createdAt ||
-          a.userId.localeCompare(b.userId)
-        );
-      }
+        case "best":
+          return (
+            b.likes.length - a.likes.length ||
+            a.dislikes.length - b.dislikes.length ||
+            b.createdAt - a.createdAt ||
+            a.userId.localeCompare(b.userId)
+          );
 
-      // Sort by dislikes, then by date, then by userId
-      if (sortValue === "worst") {
-        return (
-          b.dislikes.length - a.dislikes.length ||
-          b.createdAt - a.createdAt ||
-          a.userId.localeCompare(b.userId)
-        );
-      }
+        case "worst":
+          return (
+            b.dislikes.length - a.dislikes.length ||
+            a.likes.length - b.likes.length ||
+            b.createdAt - a.createdAt ||
+            a.userId.localeCompare(b.userId)
+          );
 
-      // Sort by dislikes, then by date, then by userId
-      if (sortValue === "replies") {
-        return (
-          b.replies.length - a.replies.length ||
-          b.createdAt - a.createdAt ||
-          a.userId.localeCompare(b.userId)
-        );
-      }
+        case "replies":
+          return (
+            b.replies.length - a.replies.length ||
+            b.createdAt - a.createdAt ||
+            a.userId.localeCompare(b.userId)
+          );
 
-      // Default case to handle unexpected sort values
-      return 0;
+        default:
+          return 0;
+      }
     });
 
     setComments([...sortedComments]);
@@ -53,78 +57,41 @@ export default function Sorter({ comments, setComments }) {
 
   useEffect(() => {
     function handleClickOutside(event) {
-      // Check if sorter is active and if user clicks outside of sorter
       if (sorterRef.current && !sorterRef.current.contains(event.target)) {
         setShowSort(false);
       }
     }
 
     window.addEventListener("click", handleClickOutside);
-
     return () => {
       window.removeEventListener("click", handleClickOutside);
     };
-  }, [sorterRef]);
+  }, []);
 
   return (
     <div className="relative w-fit" ref={sorterRef}>
       <button
         className="flex cursor-pointer items-center gap-1"
-        onClick={() => {
-          setShowSort(!showSort);
-        }}
+        onClick={() => setShowSort(!showSort)}
       >
         <FontAwesomeIcon icon={faSort} />
         <p>Sort by</p>
       </button>
 
       <div
-        className={`absolute right-0 left-0 w-fit overflow-hidden rounded-lg bg-white shadow-lg transition-all duration-300 ease-in-out ${showSort ? "max-h-screen p-2" : "max-h-0"}`}
+        className={`absolute right-0 left-0 w-fit overflow-hidden rounded-lg bg-white shadow-lg transition-all duration-300 ease-in-out ${
+          showSort ? "max-h-screen p-2" : "max-h-0"
+        }`}
       >
-        <button
-          className="w-full rounded-sm px-3 py-1 text-left transition-all hover:bg-gray-300"
-          onClick={() => {
-            sortComments("newest");
-          }}
-        >
-          Newest
-        </button>
-
-        <button
-          className="w-full rounded-sm px-3 py-1 text-left transition-all hover:bg-gray-300"
-          onClick={() => {
-            sortComments("oldest");
-          }}
-        >
-          Oldest
-        </button>
-
-        <button
-          className="w-full rounded-sm px-3 py-1 text-left transition-all hover:bg-gray-300"
-          onClick={() => {
-            sortComments("best");
-          }}
-        >
-          Best
-        </button>
-
-        <button
-          className="w-full rounded-sm px-3 py-1 text-left transition-all hover:bg-gray-300"
-          onClick={() => {
-            sortComments("worst");
-          }}
-        >
-          Controversial
-        </button>
-
-        <button
-          className="w-full rounded-sm px-3 py-1 text-left transition-all hover:bg-gray-300"
-          onClick={() => {
-            sortComments("replies");
-          }}
-        >
-          Replied to
-        </button>
+        {sortOptions.map((option) => (
+          <button
+            key={option.value}
+            className="w-full rounded-sm px-3 py-1 text-left transition-all hover:bg-gray-300"
+            onClick={() => sortComments(option.value)}
+          >
+            {option.label}
+          </button>
+        ))}
       </div>
     </div>
   );
